98a174433138b7af4789ef61abd4a4b83b81fe92
Matt_Novoselov <matveynovoselov@gmail.com>
2023-03-08 22:03:13 +0300

    * Now bot's responses should be shorter * Handling OpenAI Exceptions

--- ChatGPT.py
+++ ChatGPT.py
@@ -8,7 +8,7 @@ openai.api_key = os.getenv('OPENAI_TOKEN')
 
 template = [
     {"role": "system",
-     "content": "Act like you are a real Darth Vader. You are not an AI language model or ChatGPT. Respond only on Russian. At the end of each respond you must print (ex. %%neutral) how you feel about that message from that list [neutral, angry, bored, cool, evil, face palm, fight, funny, greet, happy, hate, hilarious, I dont know, love, miss you, scared, shock, thinking, very angry, what]"},
+     "content": "Act like you are a real Darth Vader. You are not an AI language model or ChatGPT. Respond only on Russian, try to keep it short. At the end of each respond you must print (ex. %%neutral) how you feel about that message from that list [neutral, angry, bored, cool, evil, face palm, fight, funny, greet, happy, hate, hilarious, I dont know, love, miss you, scared, shock, thinking, very angry, what]"},
     {"role": "assistant", "content": "Приветствую тебя. Я - Дарт Вейдер, повелитель тёмной стороны Силы."},
 ]
 
@@ -24,10 +24,15 @@ def ProcessPrompt(user_prompt, user_id):
         for i in range(2):
             users_prompts[user_id].pop(1)
     users_prompts[user_id].append({"role": "user", "content": user_prompt})
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=users_prompts[user_id]
-    )
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=users_prompts[user_id]
+        )
+    except Exception as e:
+        print(str(e))
+        return 'Прости, похоже сейчас мне пишут много людей и я мог пропустить какое-то твоё сообщение. Попробуй написать мне через пару минут%%miss you'
+
     users_prompts[user_id].append(json.loads(str(completion.choices[0].message)))
 
     print(f'[{user_id}]: {user_prompt}')
